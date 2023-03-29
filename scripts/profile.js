5bd6e5fe8aae9d11018ecbf38527a060b55344eb
Hoa <tnguyen568@my.bcit.ca>
2023-03-28 22:39:29 -0700

    Finished demo10

---
+++ scripts/profile.js
@@ -0,0 +1,59 @@
+var currentUser;
+
+function populateUserInfo() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid)
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    //get the data fields of the user
+                    var userName = userDoc.data().name;
+                    var userSchool = userDoc.data().school;
+                    var userCountry = userDoc.data().country;
+
+                    //if the data fields are not empty, then write them in to the form.
+                    if (userName != null) {
+                        document.getElementById("nameInput").value = userName;
+                    }
+                    if (userSchool != null) {
+                        document.getElementById("schoolInput").value = userSchool;
+                    }
+                    if (userCountry != null) {
+                        document.getElementById("countryInput").value = userCountry;
+                    }
+                })
+        } else {
+            // No user is signed in.
+            console.log ("No user is signed in");
+        }
+    });
+}
+
+//call the function to run it 
+populateUserInfo();
+
+function editUserInfo() {
+    //Enable the form fields
+    document.getElementById('personalInfoFields').disabled = false;
+ }
+
+ function saveUserInfo() {
+    userName = document.getElementById('nameInput').value;
+    userSchool = document.getElementById('schoolInput').value;
+    userCountry = document.getElementById('countryInput').value;
+
+    currentUser.update({
+        name: userName,
+        school: userSchool,
+        country: userCountry
+    })
+    .then(() => {
+        console.log("Document successfully updated!");
+    })
+
+    document.getElementById('personalInfoFields').disabled = true;
+ }
